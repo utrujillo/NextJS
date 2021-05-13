b8c1b43d482314c660802509f1baaf2610b9f338
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-05-13 14:00:58 -0500

    CSS global 	modified:   components/Layout/Layout.tsx 	modified:   pages/_app.tsx 	new file:   style.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
 import Layout from '@components/Layout/Layout'
 
+import '../style.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   // Se crean providers
   // Se crean layouts
