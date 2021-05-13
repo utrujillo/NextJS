274c1967aa1c1cfe3f0b3e89aca37a0e4cd2b257
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-05-12 21:14:21 -0500

    Integrando una API simulando la lectura de la base de datos 	modified:   database/data.ts 	modified:   database/db.ts 	new file:   package-lock.json 	new file:   pages/api/avo/index.ts

---
+++ pages/api/avo/index.ts
@@ -0,0 +1,24 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import DB from '@database'
+
+const allAvos = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    
+    const db = new DB()
+    const allEntries = await db.getAll()
+    const length = allEntries.length
+  
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ length, data: allEntries }))
+  
+  } catch (e) {
+    console.error(e)
+    res.statusCode = 500
+    res.end(
+      JSON.stringify({ length: 0, data: [], error: 'Something went wrong' })
+    )
+  }
+}
+
+export default allAvos
